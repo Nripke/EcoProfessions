c99588afc3e41bad566a215995c9d35d8cc654dd
Roxo48 <62668444+Roxo48@users.noreply.github.com>
2022-06-19 23:57:59 -0400

    Adding and Fixing

--- src/EcoProfPackage/CustomItems/ItemListener.java
+++ src/EcoProfPackage/CustomItems/ItemListener.java
@@ -8,6 +8,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.Locale;
 import java.util.Objects;
@@ -21,13 +23,20 @@ public class ItemListener implements Listener {
         this.plugin = plugin;
     }
         @EventHandler
-        public void onSwing(PlayerInteractEvent e){
-            if(!e.hasItem()){
+        public void onSwing(PlayerInteractEvent e) {
+            if (!e.hasItem()) {
                 return;
             }
-            if(!e.getItem().hasItemMeta()){
+            if (!e.getItem().hasItemMeta()) {
                 return;
             }
+            expodingPick(e);
+            emeraldPick(e);
+
+        }
+
+
+                    public void expodingPick(PlayerInteractEvent e){
                 if (e.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
                     Player player = e.getPlayer();
                     World world = player.getWorld();
@@ -38,7 +47,24 @@ public class ItemListener implements Listener {
                         plugin.getManager().getPD(player).getSH().getMin().addXP(50);
                     }
                     return;
-                        }
+                }
 
-                    }
+            }
+    public void emeraldPick(PlayerInteractEvent e){
+        if (e.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+            Player player = e.getPlayer();
+            World world = player.getWorld();
+            String itemsName = player.getInventory().getItemInMainHand().getItemMeta().getDisplayName();
+            if (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_PICKAXE && itemsName.equals("Emerald Pick")) {
+                Location location = player.getLocation();
+                PotionEffect potionEffect = new PotionEffect(PotionEffectType.FAST_DIGGING, 2, 4, false,false,false);
+                player.addPotionEffect(potionEffect);
+            }
+            return;
         }
+
+    }
+
+
+
+}
